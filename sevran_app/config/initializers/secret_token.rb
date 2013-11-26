1b5e01f11148cdaa021cc24810784ed46630ff49
Aladine <alazaiane@yahoo.fr>
2013-11-26 14:52:11 +0100

    fin de l'intro de l'app

---
+++ sevran_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SevranApp::Application.config.secret_token = '15bb3edae5ad919aefd50a5cc65ef6aa26342671e3c55e0d84a425e531c2d77453718a8a2a3a609e23a40ef3e43b92141ba4ce7ff68050cee8d4bd554f3c9beb'
